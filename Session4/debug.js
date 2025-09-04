0192add3ea3a6a279b4933166dc466e6d0379eea
Parthpatil19 <parthpatil1902@gmail.com>
2025-09-04 19:47:40 +0530

    Added Debugging file

---
+++ Session4/debug.js
@@ -0,0 +1,22 @@
+//Debug - 1 
+
+function percentage(a,b){
+
+    let result = (a/b)*100;
+    return result;
+
+}
+
+let percent = percentage(30,100);
+console.log("Avg. percentage :" + percent);
+
+//Debug - 2
+
+function add(m,n){
+
+    let res = m+n;
+    return res;
+
+}
+let sum = add(5,5);
+console.log("Summation : " + sum);
